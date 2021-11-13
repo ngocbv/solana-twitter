583396a8a50bcfb696700f6e7bc92a1c780d56f4
Loris Leiva <loris.leiva@gmail.com>
2021-11-13 23:46:09 +0000

    wip

--- app/src/components/WorkspaceProvider.vue
+++ app/src/components/WorkspaceProvider.vue
@@ -1,6 +1,19 @@
 <script setup>
-import { initWorkspace } from '@/composables'
-initWorkspace()
+import { initWorkspace } from '../useWorkspace'
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    network: {
+        type: String,
+        default: 'http://127.0.0.1:8899',
+    },
+    commitment: {
+        type: String,
+        default: 'processed',
+    },
+})
+
+initWorkspace(props.network, props.commitment)
 </script>
 
 <template>
