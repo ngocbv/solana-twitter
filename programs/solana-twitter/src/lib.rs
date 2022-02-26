8ede5b578f18b047bc52cf193251ecdea2fba348
ngocbv <ngocbv@remitano.com>
2022-02-26 23:00:39 +0700

    Change anchor version from 0.22.0 to 0.21.0

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::error::*;
+use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_program;
 
 declare_id!("BsWfCtugVqewDAKuFjQBvmnWwXUA2m2n8K3wrJK2NiCq");
@@ -7,8 +7,7 @@ declare_id!("BsWfCtugVqewDAKuFjQBvmnWwXUA2m2n8K3wrJK2NiCq");
 #[program]
 pub mod solana_twitter {
     use super::*;
-
-    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> Result<()> {
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
         let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
         let author: &Signer = &ctx.accounts.author;
         let clock: Clock = Clock::get().unwrap();
@@ -17,7 +16,7 @@ pub mod solana_twitter {
             return Err(ErrorCode::TopicTooLong.into())
         }
 
-        if content.chars().count()  > 280 {
+        if content.chars().count() > 280 {
             return Err(ErrorCode::ContentTooLong.into())
         }
 
@@ -28,6 +27,27 @@ pub mod solana_twitter {
 
         Ok(())
     }
+
+    pub fn update_tweet(ctx: Context<UpdateTweet>, topic: String, content: String) -> ProgramResult {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
+
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
+
+        tweet.topic = topic;
+        tweet.content = content;
+
+        Ok(())
+    }
+
+    pub fn delete_tweet(_ctx: Context<DeleteTweet>) -> ProgramResult {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -35,9 +55,24 @@ pub struct SendTweet<'info> {
     #[account(init, payer = author, space = Tweet::LEN)]
     pub tweet: Account<'info, Tweet>,
     #[account(mut)]
-    pub author:  Signer<'info>,
+    pub author: Signer<'info>,
     #[account(address = system_program::ID)]
+    /// CHECK:
     pub system_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateTweet<'info> {
+    #[account(mut, has_one = author)]
+    pub tweet: Account<'info, Tweet>,
+    pub author: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct DeleteTweet<'info> {
+    #[account(mut, has_one = author, close = author)]
+    pub tweet: Account<'info, Tweet>,
+    pub author: Signer<'info>,
 }
 
 #[account]
@@ -49,18 +84,18 @@ pub struct Tweet {
 }
 
 const DISCRIMINATOR_LENGTH: usize = 8;
-const PUBLIC_KEY_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
 const TIMESTAMP_LENGTH: usize = 8;
-const STRING_LENGTH_PREFIX: usize = 4;
-const MAX_TOPIC_LENGTH: usize = 50 * 4;
-const MAX_CONTENT_LENGTH: usize = 280 * 4;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the size of the string.
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars max.
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 280 chars max.
 
 impl Tweet {
     const LEN: usize = DISCRIMINATOR_LENGTH
-        + PUBLIC_KEY_LENGTH
-        + TIMESTAMP_LENGTH
-        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH
-        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH;
+        + PUBLIC_KEY_LENGTH // Author.
+        + TIMESTAMP_LENGTH // Timestamp.
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH // Topic.
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH; // Content.
 }
 
 #[error]
